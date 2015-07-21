31351ca16b02860f850e7410be060de05d0c2b62
Simon Lees <simon@simotek.net>
2015-07-21 21:52:46 +0930

    Exp: move first python websockets there

--- src/exp/threadedwebsocket.py
+++ src/exp/threadedwebsocket.py
@@ -18,23 +18,22 @@
 
 import threading
 import time
-import serial
-
+import websockets
 
 class  ThreadedWebsocket(threading.Thread):
 
-  def __init__(self, port=None, baud=None):
+  def __init__(self, hostname=None, port=None):
     # First set up thread related  code
     threading.Thread.__init__(self)
     self.__dataLock = threading.RLock()
     self.__stopRunning = False
     self.__finished =  True
 
-    # now set up serial related code
-    self.__serialPort = port
-    self.__serialBaud = baud
-    self.__serialRecieveFunc = None
-    self.__serialConnection = None
+    # now set up socket related code
+    self.__hostname = hostname
+    self.__port = port
+    self.__recieveFunc = None
+    self.__connection = None
 
   def __del__(self):
     if self.__stopRunning:
@@ -57,22 +56,24 @@ class  ThreadedWebsocket(threading.Thread):
 
   # This method takes a function as a paramater,
   # that function should take 1 paramater which will be passed a line of serial data
-  def setSerialRecieveFunction(self, RecvFunc):
+  def setRecieveFunction(self, RecvFunc):
     self.__dataLock.acquire()
-    self.__serialRecieveFunc = RecvFunc
+    self.__recieveFunc = RecvFunc
     self.__dataLock.release()
 
-  def create(self):
-    # Create Serial connection - use a long timeout of 999 as its running in a thread and could
-    # really wait forever for readline
-    # TBD: handle exception
+  def createServer(self):
     self.__dataLock.acquire()
-    self.__serialConnection = serial.Serial(self.__serialPort, self.__serialBaud, timeout=None)
+    self.__connection = serial.Serial(self.__serialPort, self.__serialBaud, timeout=None)
+    self.__dataLock.release()
+
+  def createClient(self):
+    self.__dataLock.acquire()
+    self.__connection = serial.Serial(self.__serialPort, self.__serialBaud, timeout=None)
     self.__dataLock.release()
 
   # writes data to the serial port, automatically re encodes data to
   def write(self, data):
-    self.__serialConnection.write(data)
+    self.__connection.send(data)
     self.__serialConnection.flush()
 
   # overloads the theads run class to provide non blocking Seral responses
@@ -94,14 +95,12 @@ class  ThreadedWebsocket(threading.Thread):
         self.__dataLock.release()
 
       # this code blocks until it recieves a new line char
-      read = self.__serialConnection.readline()
-
-      decoded = str(read.decode('utf-8', errors='ignore').rstrip().replace('\n', '').replace('\r', ''))
+      read = self.__connection.recv()
 
       # check whatever you need to check here
       if self.__serialRecieveFunc is not None:
         # Call the function passing it the line as a parami
-        self.__serialRecieveFunc(decoded)
+        self.__serialRecieveFunc(read)
       time.sleep(0.5)
 
   def stop(self):
