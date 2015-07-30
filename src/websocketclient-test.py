d6ad23adbbd76c7c67eced45eeb686e58821b420
Simon Lees <simon@simotek.net>
2015-07-30 22:29:49 +0930

    Python: Don't need old client test

--- src/websocketclient-test.py
+++
@@ -1,7 +0,0 @@
-from PyLibs.websocketclient import WebsocketClient
-
-client = WebsocketClient('ws://localhost:8765/')
-client.start()
-print("Post start")
-client.write("Hello world")
-print("post write")
